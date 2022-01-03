eb2e6586b5107f31a79d507c42deb093c9577adb
Eriau <mathieu.eriau@hotmail.Fr>
2022-01-03 10:58:35 +0100

    Yes

--- passerelleEmergency.py
+++ passerelleEmergency.py
@@ -5,21 +5,16 @@
 
 import serial
 import json
-import paho.mqtt.client as mqtt
 import time
 
 '''
  * variables for script
 '''
 
-FILENAME        = "values.txt"
-LAST_VALUE      = "deadbeef"
 SERIALPORT      = "COM5"
 BAUDRATE        = 115200
 
 ser = serial.Serial()
-client = mqtt.Client("", True)
-queueMessage = []
 
 # -------------------- Functions -------------------- #
 '''
@@ -39,44 +34,9 @@ def initUART():
     try:
         ser.open()
         print("Starting Up Serial Monitor")
-    except serial.SerialException:
+    except serial.SerialException as e:
         print(f"Serial {SERIALPORT} port not available")
         exit()
-
-'''
- * init mqtt
-'''
-def initMQTT():
-    client.connect("127.0.0.1", 1883, 60)
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.loop_start()
-
-'''
- * function on connect for mqtt broker
-'''
-def on_connect(client, userdata, flags, rc):
-  print("Connected with result code "+str(rc))
-  client.subscribe("topic/detectors")
-
-'''
- * function on message for mqtt broker
-'''
-def on_message(client, userdata, msg): # MANQUE LA LOGIQUE DE VERIF DES MESSAGES POUR RENVOYER ET TOUT
-    #queueMessage.append(msg.payload)
-    print(msg.payload)
-    sendUARTMessage(msg.payload)
-    '''
-    for i in range (5):
-        msgRcvd = readUARTMessage()
-        if (msgRcvd == "MESSAGE_SUCCESS"):
-            queueMessage.pop(0)
-            return 1
-        else:
-            time.sleep(0.5)
-    print("TIMEOUT for msg : <{queueMessage[0]}>")
-    queueMessage.pop(0)
-    '''
 
 '''
  * send message to microcontroller with serial
@@ -84,13 +44,13 @@ def on_message(client, userdata, msg): # MANQUE LA LOGIQUE DE VERIF DES MESSAGES
 def sendUARTMessage(msg):
     ser.write(msg)
     print(f"Message <{msg.decode()}> sent to micro-controller")
+    time.sleep(0.5)
 
 '''
  * receive message from serial
 '''
 def readUARTMessage():
     msg = ser.readline()
-    ser.flush()
     packet = msg.decode()
     return packet
 
@@ -99,7 +59,6 @@ def readUARTMessage():
 '''
 if __name__ == '__main__':
     initUART()
-    initMQTT()
 
     print ('Press Ctrl-C to quit.')
 
@@ -110,6 +69,5 @@ if __name__ == '__main__':
             if (ser.inWaiting() > 0): # if incoming bytes are waiting
                 print(readUARTMessage())
     except (KeyboardInterrupt, SystemExit):
-        client.disconnect()
         ser.close()
         exit()
